43121d92174761a389fb907fb65f7473d75f8b34
Kirill Matrosov <k.matrosov@orbita.center>
2018-10-03 21:08:22 +0300

    refactor read_from_txt

--- scripts/read_from_txt.py
+++ scripts/read_from_txt.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 import re
-from hashlib import sha256
-
-
-def hash(text):
-    hashed = sha256(text.encode("UTF-8")).hexdigest()
-    return hashed
-
 
 f = open('../data1.txt', 'r')
 data = f.readlines()
@@ -19,11 +12,10 @@ mapXtoWR = {}  # CTR
 
 for line in data:
     x = re.findall(r'(\S+)', line)
-    # key = hash(x[4] + "_" + x[5] + "_" + x[6])
-    key = x[4] + "_" + x[5] + "_" + x[6] + "_" + x[2] + "_" + x[3]
+    key = x[4] + "_" + x[5] + "_" + x[6] + "_" + x[2] + "_" + x[3] # city sex age camp adv
     if key in mapXtoWR:
-        mapXtoWR[key][0] += int(x[0])
-        mapXtoWR[key][1] += int(x[1])
+        mapXtoWR[key][0] += int(x[0]) # clicks
+        mapXtoWR[key][1] += int(x[1]) # shows
     else:
         mapXtoWR[key] = [int(x[0]), int(x[1]), 0]
 
