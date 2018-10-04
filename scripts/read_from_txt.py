ebde46a422e5e5599ac01dbc309d2d775474ed6a
Kirill Matrosov <k.matrosov@orbita.center>
2018-10-04 20:15:53 +0300

    two

--- scripts/read_from_txt.py
+++ scripts/read_from_txt.py
@@ -12,22 +12,22 @@ mapXtoWR = {}  # CTR
 
 for line in data:
     x = re.findall(r'(\S+)', line)
-    key = x[4] + "_" + x[5] + "_" + x[6] + "_" + x[2] + "_" + x[3] # city sex age camp adv
+    key = x[4] + "_" + x[5] + "_" + x[6] + "_" + x[2] + "_" + x[3]  # city sex age camp adv
     if key in mapXtoWR:
-        mapXtoWR[key][0] += int(x[0]) # clicks
-        mapXtoWR[key][1] += int(x[1]) # shows
+        mapXtoWR[key][0] += int(x[0])  # clicks
+        mapXtoWR[key][1] += int(x[1])  # shows
     else:
-        mapXtoWR[key] = [int(x[0]), int(x[1]), 0]
+        mapXtoWR[key] = [int(x[0]), int(x[1]), 0, x[2], x[3]]
 
-line = "{:>5}\t {:>4}\t {:<4}% {:>30}\t" \
-    .format("click", "show", "CTR", "user")
+line = "{:>5}\t {:>4}\t {:<4}% {:>4}\t {:>4}\t {:>30}\t" \
+    .format("click", "show", "CTR", "campId", "advId", "user")
 f.write(line + "\n")
 
 for key in mapXtoWR:
     if mapXtoWR[key][1] > 0:
         mapXtoWR[key][2] = round(10000 * mapXtoWR[key][0] / mapXtoWR[key][1]) / 100
-    line = "{:>5}\t {:>4}\t {:<6.2f} {:>30}" \
-        .format(mapXtoWR[key][0], mapXtoWR[key][1], mapXtoWR[key][2], key)
+    line = "{:>5}\t {:>4}\t {:<6.2f} {:>4}\t {:>4}\t {:>30}" \
+        .format(mapXtoWR[key][0], mapXtoWR[key][1], mapXtoWR[key][2], mapXtoWR[key][3], mapXtoWR[key][4], key)
     f.write(line + "\n")
 
 f.close()
--- scripts/run.py
+++ scripts/run.py
@@ -15,10 +15,11 @@ f.close()
 windata = []
 for line in data:
     x = re.findall(r'(\S+)', line)
-    windata.append([x[0], x[1], x[2], x[3]])
+    windata.append([x[0], x[1], x[2], x[3], x[4], x[5]])
 
 env = gym.make('Rtb-v0')
 env.setState(t=1000, b=300)
+
 
 # сделать подгрузку из файла по куче моделей
 def test_one(env, num_episodes=300):
@@ -71,5 +72,57 @@ def test_one(env, num_episodes=300):
     return 0
 
 
-test_one(env)
+# сделать подгрузку из файла по куче моделей
+def test_two(env, num_episodes=300, campId='1', advId='1'):
+    __windata = list(filter(lambda x: x[3] == campId and x[4] == advId, windata))
+    model = Sequential()
+    model.add(InputLayer(batch_input_shape=(1, 2)))
+    model.add(Dense(300, activation='sigmoid'))
+    model.add(Dense(700, activation='linear'))
+    model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 
+    acc = []
+    for i in range(num_episodes):
+        minBid = 3.
+        maxBid = 10.
+        s = env.reset()
+        done = False
+        y = 0.95
+        eps = 0.5
+        decay_factor = 0.999
+        delta = 0.01
+        if i % 10 == 0:
+            print("Episode {} of {}".format(i + 1, num_episodes))
+        count = 0
+        sum = 0
+        while not done:
+            count += 1
+            stateNP = np.array([s['t'], s['b']]).reshape(1, 2)
+            if np.random.random() < eps:
+                bid = np.random.randint(0, int((maxBid - minBid) / delta)) * np.random.randint(0, 2)
+            else:
+                bid = np.argmax(model.predict(stateNP))
+            # необходимо рандомить по одному объявлению, а не по всем сразу
+            new_s, r, done, _ = env.step(minBid + bid * delta,
+                                         float(__windata[(np.random.randint(0, len(__windata)))][2]))
+
+            new_stateNP = np.array([new_s['t'], new_s['b']]).reshape(1, 2)
+
+            target = r + y * np.max(model.predict(new_stateNP))
+            target_vec = model.predict(stateNP)[0]
+            target_vec[bid] = target
+            model.fit(stateNP, target_vec.reshape(-1, 700), epochs=1, verbose=0)
+            s = new_s
+            # print(r, round(minBid + bid * delta, 2), s['b'])
+            if (s['b'] < minBid):
+                done = True
+            sum += r
+        acc.append(sum / count)
+    plt.plot(acc)
+    plt.ylabel('Average reward per game')
+    plt.xlabel('Number of games')
+    plt.show()
+    return 0
+
+
+test_two(env)
