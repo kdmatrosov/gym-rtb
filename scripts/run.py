c4ad422ea3ac8706bd0abac9e0570463e35263de
Kirill Matrosov <k.matrosov@orbita.center>
2018-10-05 19:57:09 +0300

    train_model

--- scripts/run.py
+++ scripts/run.py
@@ -8,6 +8,7 @@ from dec_keras import Dec_Keras
 from keras.models import Sequential
 from keras.layers import Dense, InputLayer
 import matplotlib.pylab as plt
+import datetime
 
 f = open('../winrate.txt', 'r')
 data = f.readlines()
@@ -22,6 +23,8 @@ env = gym.make('Rtb-v0')
 env.setState(t=1000, b=300)
 
 decKeras = Dec_Keras()
+
+delta = 0.01
 
 
 # сделать подгрузку из файла по куче моделей
@@ -41,7 +44,6 @@ def test_one(env, num_episodes=300):
         y = 0.95
         eps = 0.5
         decay_factor = 0.999
-        delta = 0.01
         if i % 10 == 0:
             print("Episode {} of {}".format(i + 1, num_episodes))
         count = 0
@@ -76,11 +78,12 @@ def test_one(env, num_episodes=300):
 
 
 # сделать подгрузку из файла по куче моделей
-def test_two(env, num_episodes=1, campId='1', advId='1'):
+def test_two(env, num_episodes=1000, campId='1', advId='1'):
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
     __windata = list(filter(lambda x: x[3] == campId and x[4] == advId, windata))
     model = Sequential()
-
     model.add(InputLayer(batch_input_shape=(1, 2)))
+    model.add(Dense(300, activation='sigmoid'))
     model.add(Dense(300, activation='sigmoid'))
     model.add(Dense(700, activation='linear'))
     model.compile(loss='mse', optimizer='adam', metrics=['mae'])
@@ -94,7 +97,6 @@ def test_two(env, num_episodes=1, campId='1', advId='1'):
         y = 0.95
         eps = 0.5
         # decay_factor = 0.999
-        delta = 0.01
         if i % 10 == 0:
             print("Episode {} of {}".format(i + 1, num_episodes))
         count = 0
@@ -123,6 +125,7 @@ def test_two(env, num_episodes=1, campId='1', advId='1'):
             sum += r
         acc.append(sum / count)
     decKeras.saveWeights(model, campId + "_" + advId)
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
     plt.plot(acc)
     plt.ylabel('Average reward per game')
     plt.xlabel('Number of games')
@@ -130,4 +133,65 @@ def test_two(env, num_episodes=1, campId='1', advId='1'):
     return 0
 
 
-test_two(env)
+def train_model(env, campId='1', advId='1', minBid=3., maxBid=10., num_episodes=100):
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+    __windata = list(filter(lambda x: x[3] == campId and x[4] == advId, windata))
+    model = Sequential()
+    nnMin = int(minBid / delta)
+    nnMax = int(maxBid / delta)
+    model.add(Dense(nnMin, activation='sigmoid'))
+    model.add(Dense(nnMin, activation='sigmoid'))
+    model.add(Dense(nnMax, activation='linear'))
+    model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+    model = decKeras.loadWeights(model, campId + "_" + advId)
+    model = decKeras.loadWeights(model, campId + "_" + advId)
+    acc = []
+    for i in range(num_episodes):
+        s = env.reset()
+        done = False
+        y = 0.95
+        eps = 0.5
+        # decay_factor = 0.999
+        if i % 10 == 0:
+            print("Episode {} of {}".format(i + 1, num_episodes))
+        count = 0
+        sum = 0
+        while not done:
+            count += 1
+            stateNP = np.array([s['t'], s['b']]).reshape(1, 2)
+            if np.random.random() < eps:
+                bid = np.random.randint(0, int((maxBid - minBid) / delta)) * np.random.randint(0, 2)
+            else:
+                bid = np.argmax(model.predict(stateNP))
+            new_s, r, done, _ = env.step(minBid + bid * delta,
+                                         float(__windata[(np.random.randint(0, len(__windata)))][2]))
+
+            new_stateNP = np.array([new_s['t'], new_s['b']]).reshape(1, 2)
+
+            target = r + y * np.max(model.predict(new_stateNP))
+            target_vec = model.predict(stateNP)[0]
+            target_vec[bid] = target
+            model.fit(stateNP, target_vec.reshape(-1, nnMax), epochs=1, verbose=0)
+            s = new_s
+            # print(r, round(minBid + bid * delta, 2), s['b'])
+            if (s['b'] < minBid):
+                done = True
+            sum += r
+        acc.append(sum / count)
+    decKeras.saveWeights(model, campId + "_" + advId)
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+    plt.plot(acc)
+    plt.ylabel('Average reward per game')
+    plt.xlabel('Number of games')
+    plt.show()
+    return 0
+
+
+def callback_yad(env, num_episodes=1, campId='1', advId='1'):
+    bid = 0.0
+    return bid
+
+
+for campX in range(1, 2):
+    for advX in range(1, 2):
+        train_model(env, str(campX), str(advX))
