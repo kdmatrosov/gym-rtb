b5374b3b48de4eb503bde0ee1936520b53f8dfc6
Kirill Matrosov <k.matrosov@orbita.center>
2018-11-12 20:36:51 +0300

    minutes/hours

--- scripts/charts.py
+++ scripts/charts.py
@@ -1,13 +1,52 @@
 from random import randint
 import matplotlib.pylab as plt
+
 print(randint(0, 9))
 acc = []
-for number in range(60):
-    num = randint(0, 4) if randint(0, 7) == 0 else 0
+
+for number in range(60 * 24):
+    max = 30
+    rate = 100
+    if number < 500:
+        max = 10
+        if number > 100 and number < 300:
+            max = 5
+    else:
+        max = 15 + randint(-1, 2)
+        if number > 1000:
+            max = 22
+        if number > 1000:
+            max = 30
+        if number > 1150:
+            max = 27
+        if number > 1200:
+            max = 25
+        if number > 1250:
+            max = 20
+        if number > 1300:
+            max = 15
+        if number > 1350:
+            max = 12
+
+    num = randint(-1 + int(max / 5), max + randint(-4, 5)) if randint(-1 + int(max / 5), rate) != 0 else 0
     acc.append(num)
     print(num)
 
 plt.plot(acc)
 plt.ylabel('Клики')
-plt.xlabel('Секунды')
+plt.xlabel('Минуты')
+plt.show()
+
+acc2 = []
+i = 0
+for number in range(0, 24):
+    sum = 0
+    for j in range(0, 60):
+        sum = sum + acc[i]
+        i = i + 1
+    acc2.append(sum)
+
+plt.plot(acc2)
+plt.ylabel('Клики')
+plt.xlabel('Часы')
 plt.show()
